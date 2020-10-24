af67ffd38a8823b8f9f678eccdf02a3092f3516a
Padmasri <padhu.9829@gmail.com>
2020-10-24 16:45:33 +0530

    Updates

---
+++ precache-manifest.e678313acb9180a580c3a0831a43dc73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f120f31d6d7644d3dba2263dd30d47",
+    "url": "/vividly/index.html"
+  },
+  {
+    "revision": "567865ccb9d4e702b1cc",
+    "url": "/vividly/static/js/2.8bed6cab.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/vividly/static/js/2.8bed6cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c791b6dad5676011f55d",
+    "url": "/vividly/static/js/main.e4a3dba5.chunk.js"
+  },
+  {
+    "revision": "abe6dd651558114aeb27",
+    "url": "/vividly/static/js/runtime-main.c670fe23.js"
+  }
+]);

2f750882059d20effdb6ed680d46534018e760f7
Padmasri <padhu.9829@gmail.com>
2020-10-24 21:53:16 +0530

    update

--- src/pages/Corporate/Data.js
+++ src/pages/Corporate/Data.js
@@ -1,6 +1,6 @@
 export const headData={
-    headline:'Get in touch with us with your requirements, we will give you a story/storytelling session that caters to your audience.',
-    subtitle:'',
+    headline:'Get personal touch in every storytelling session',
+    subtitle:'Get in touch with us with your requirements, we will give you a story/storytelling session that caters to your audience.',
     image: 'vividly/images/corporate.svg',
     alt:'bestseller',
     buttontext:'Contact us',

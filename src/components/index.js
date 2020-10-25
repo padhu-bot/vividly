59c64e7080604a505b26aef03b7560629a0c70b3
Padmasri <padhu.9829@gmail.com>
2020-10-25 18:45:40 +0530

    build update

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export {default as Footer} from './Footer/Footer';
 export {default as Steps} from './Steps/Steps';
 export {default as PriceCard} from './PriceCard/PriceCard';
 export {default as Testimonial} from './Testimonial/Testimonial';
+export {default as PriceCardhome} from './PriceCard/PriceCardhome';

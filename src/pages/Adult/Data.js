de9368e2470eaf28cdd54f6e56993b61ae4ea781
Padmasri <padhu.9829@gmail.com>
2020-11-08 14:57:31 +0530

    updated adult section

--- src/pages/Adult/Data.js
+++ src/pages/Adult/Data.js
@@ -1,6 +1,6 @@
 export const headData={
-    headline:"Don't limit yourself.",
-    subtitle:'Spread the love and enjoy every day reading great books.',
+    headline:"Want to gift the perfect memory for your near and dear ones ? ",
+    subtitle:'We recreate your memories into a wonderful book featuring YOU !',
     image: 'vividly/images/team.svg',
     alt:'bestseller',
     buttontext:'Explore',
@@ -12,22 +12,22 @@ export const bodyData1={
     book1:[{
         id:'M1',
         name:'',
-        title:'',
-        rate:'Rs.200',
-        brief:""
+        title:'Just give me a reason',
+        rate:'Rs.2500',
+        brief:"This story speaks of the story of a lovely couple from how they met to their marriage ! "
     }],
     book2:[{
         id:'M2',
         name:'',
-        title:'',
-        rate:'Rs.200',
-        brief:""
+        title:"Mother's day poster",
+        rate:'Rs.2500',
+        brief:"Create your own super heros"
     }],
     book3:[{
         id:'M3',
         name:'',
-        title:'',
-        rate:'Rs.200',
-        brief:""
+        title:"Mother's day poster",
+        rate:'Rs.2500',
+        brief:"Create your own super heros"
     }],
 }
